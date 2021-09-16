9fabb92a3341e8a0e11ee6e1af25da7f0a7b294b
RoboVedh <89797345+RoboVedh@users.noreply.github.com>
2021-09-16 21:18:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+from telegram.ext import Updater, MessageHandler, Filters
+
+from Adafruit_IO import Client
+ 
+aio = Client('Robo_Vedh','aio_YcGC58BdtKxDSEOfdKN7C7dTmqqG')
+ 
+def vedh1(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('light turned on')
+  aio.send('telebot',1)
+ 
+def vedh2(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('light turned off')
+  aio.send('telebot',0)
+ 
+def vedh3(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Fan turned on')
+  aio.send('telebot',4)
+ 
+def vedh4(bot,update):
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Fan turned off')
+  aio.send('telebot',2)
+ 
+ 
+def main(bot,update):
+  a= bot.message.text.lower()
+  
+  if a =="turn on light":
+    vedh1(bot,update)
+  elif a =="turn off light":
+    vedh2(bot,update)
+  elif a =="turn on fan":
+    vedh3(bot,update)
+  elif a =="turn off fan":
+    vedh4(bot,update) 
+     
+bot_token =  '1951115398:AAHC5RLHwhD8H2QQ_HrfRuoqb-ogUYeLY7w'
+u = Updater(bot_token,use_context=True)
+dp = u.dispatcher
+dp.add_handler(MessageHandler(Filters.text,main))
+u.start_polling()
+u.idle()
